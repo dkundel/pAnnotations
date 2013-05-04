3171b59a742e48796a0efb1d7ca6315a28a8a607
Ernesto Rodriguez <neto@netowork.me>
2013-05-04 16:20:03 +0200

    Initial streaming attempt

--- public/js/audioStreams.js
+++ public/js/audioStreams.js
@@ -1,7 +1,8 @@
 (function (exports){    
 
   var apiKey = "28173072";
-  var sessionId = "2_MX4yODE3MzA3Mn4xMjcuMC4wLjF-U2F0IE1heSAwNCAwNTo1OTozMiBQRFQgMjAxM34wLjMxNDc1NDM3fg";
+  //var sessionId = "2_MX4yODE3MzA3Mn4xMjcuMC4wLjF-U2F0IE1heSAwNCAwNTo1OTozMiBQRFQgMjAxM34wLjMxNDc1NDM3fg";
+  var sessionId = "1_MX4yODE3MzA3Mn4xMjcuMC4wLjF-U2F0IE1heSAwNCAwNjo1NDoyMyBQRFQgMjAxM34wLjcyNTE3MTE1fg";
   var token = "T1==cGFydG5lcl9pZD0yODE3MzA3MiZzZGtfdmVyc2lvbj10YnJ1YnktdGJyYi12MC45MS4yMDExLTAyLTE3JnNpZz02Y2I3NDlkZTM1ZTY2OWFlZTY4NzM4M2Q5ZmM0NjUyZTQyYjA1ODE5OnJvbGU9cHVibGlzaGVyJnNlc3Npb25faWQ9JmNyZWF0ZV90aW1lPTEzNjc2NzIzNzImbm9uY2U9MC4yMTI5MzMzMjU3ODI4NTUxOCZleHBpcmVfdGltZT0xMzY3NzU4NzcyJmNvbm5lY3Rpb25fZGF0YT0=";
 
   var publishDiv = 'stream';
@@ -24,16 +25,16 @@
   }
 
   exports.streaming.serverConnectedHandler = function(event) {
-    var publisher = TB.initPublisher(apiKey, 'myPublisherDiv');
+    var publisher = TB.initPublisher(apiKey, publishDiv);
     //var sub = TB.initSubscriber(apiKey, 'mySubDiv');
     session.publish(publisher);
     //session.subscribe(sub);
   }
 
-  exports.streaming.clientConnectHandler = function(){
+  exports.streaming.clientConnectedHandler = function(event){
 
     if(event.streams[0])
-      session.subscribe( event.streams[0], 'mySubDiv');
+      session.subscribe( event.streams[0], publishDiv);
   }
 
   $(function(){
