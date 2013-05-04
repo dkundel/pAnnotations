cf2d65b926db3a4b48c346694c0dd6ef98688e92
Stefan Mirea <steven.mirea@gmail.com>
2013-05-04 15:33:36 +0200

    styled chat

--- public/handlers/chat.js
+++ public/handlers/chat.js
@@ -1,22 +1,65 @@
 (function chat_handlers_module (handlers) {
 
-  handlers.chat = on_chat;
-
-  handlers.chat_load = function on_chat_load (socket, data) {
-    for (var i=0; i<data.messages.length; ++i) {
-      on_chat(socket, data.messages[i]);
-    }
+  var structure = {
+    wrapper: null,
+    messages: jq_element('ul'),
+    chat_wrapper: jq_element('form'),
+    chat: jq_element('input'),
+    chat_submit: jq_element('input')
   };
 
-  window.chat = function chat (message) {
+  register_socket_handlers();
+
+  $(function () {
+    make_structure();
+  });
+
+  function make_structure () {
+    structure.wrapper = $('#chat');
+    structure.wrapper.append(
+      structure.messages,
+      structure.chat_wrapper.append(
+        structure.chat.attr({type: 'text'}),
+        structure.chat_submit.attr({type: 'submit', value:'chat'})
+      )
+    );
+    structure.chat_wrapper.on('submit', function on_submit (event) {
+      event.preventDefault();
+      chat(structure.chat.val());
+      structure.chat.val('');
+    });
+  }
+
+  function register_socket_handlers () {
+    handlers.chat = function on_chat (socket, data) {
+      console.log(data);
+      add_message(data);
+    };
+
+    handlers.chat_load = function on_chat_load (socket, data) {
+      for (var i=0; i<data.messages.length; ++i) {
+        add_message(data.messages[i]);
+      }
+    };
+  }
+
+  function add_message (data) {
+    structure.messages.append(
+      '<li id="'+data.id+'" user_id="'+data.user_id+'">'+
+        '<img src="'+data.photo+'" alt="image" />'+
+        '<h4>'+data.name+'</h4>'+
+        '<div>'+data.message+'</div>'+
+        '<div class="meta">'+data.timestamp+'</div>'+
+      '</li>'
+    );
+    console.info('['+data.timestamp+'] '+data.id+': '+data.message);
+  };
+
+  function chat (message) {
     socket.emit('chat', {
       id: Math.floor(Math.random() * 10000),
       message: message
     });
   }
-
-  function on_chat (socket, data) {
-    console.info('['+data.timestamp+'] '+data.id+': '+data.message);
-  };
 
 })(window.socket_handlers);
