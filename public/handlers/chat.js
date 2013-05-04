057f8e222710ca925b7203967e0f1fc65ca258c7
Stefan Mirea <steven.mirea@gmail.com>
2013-05-04 18:41:57 +0200

    good shit

--- public/handlers/chat.js
+++ public/handlers/chat.js
@@ -7,21 +7,75 @@
     messages: jq_element('ul'),
     chat_wrapper: jq_element('form'),
     chat: jq_element('input'),
-    chat_submit: jq_element('input')
+    chat_submit: jq_element('input'),
+    revisions: jq_element('div'),
+    revisions_text_input: jq_element('input'),
+    revisions_image_input: jq_element('input')
   };
 
   var messages = [];
+
+  var objects = null;
 
   register_socket_handlers();
 
   $(function () {
     make_structure();
+    init_prezi();
   });
+
+  function init_prezi () {
+    var player = PreziControl.Prezi;
+    player.on(PreziPlayer.EVENT_STATUS, function (event) {
+      if (event.value == PreziPlayer.STATUS_CONTENT_READY){
+        player.on(PreziPlayer.EVENT_OBJECT_QUERY_RESPONSE, function(event){
+          player.off(PreziPlayer.EVENT_OBJECT_QUERY_RESPONSE);
+          objects = event.data.getObjects;
+          console.log(objects);
+          make_revisions();
+        });
+        player.getObjects();
+      }
+    });
+  }
+
+  function make_revisions () {
+    var texts = jq_element('select');
+    var images = jq_element('div');
+    objects.texts.forEach(function (text) {
+      texts.append(
+        jq_element('option').attr({
+          value: text.objectId
+        }).html(text.text)
+      );
+    });
+    texts.on('change', function on_change () {
+      PreziControl.Prezi.toObject($(this).val());
+    });
+    structure.revisions_text_input.
+      on('change', function on_change () {
+        PreziControl.Prezi.setText(texts.val(), $(this).val());
+      });
+    objects.images.forEach(function (image) {
+      images.append(
+
+      );
+    });
+    structure.revisions.append(
+      jq_element('h3').html('Add a text revision'),
+      structure.revisions_text_input.attr({type: 'text'}),
+      texts,
+      jq_element('h3').html('Add an image revision'),
+      structure.revisions_image_input.attr({type: 'text'}),
+      images
+    );
+  }
 
   function make_structure () {
     structure.wrapper = $('#chat');
     structure.wrapper.append(
       structure.navigation,
+      structure.revisions,
       structure.messages,
       structure.chat_wrapper.append(
         structure.chat.attr({type: 'text'}),
@@ -48,20 +102,33 @@
           children().
           hide().
           filter(function () {
-            return $(this).find('.prezi-go-to[slide-number="'+slide_number+'"]').length > 0
+            return $(this).
+                      find('.prezi-go-to[slide-number="'+slide_number+'"]').
+                      length > 0;
           }).
           show();
       }
     });
+    structure.revisions.addClass('revisions');
     structure.messages.on('click.prezi-go-to', '.prezi-go-to', function (event) {
       event.preventDefault();
       go_to(Number($(this).attr('slide-number')));
     });
-    structure.chat_wrapper.on('submit', function on_submit (event) {
-      event.preventDefault();
-      chat(structure.chat.val());
-      structure.chat.val('');
-    });
+    structure.chat_wrapper.
+      on('keydown', function on_keydown (event) {
+        var R_keyCode = 82;
+        if (event.keyCode == R_keyCode && event.ctrlKey) {
+          event.preventDefault();
+          event.stopPropagation();
+          event.stopImmediatePropagation();
+          structure.revisions.fadeToggle();
+        }
+      }).
+      on('submit', function on_submit (event) {
+        event.preventDefault();
+        chat(structure.chat.val());
+        structure.chat.val('');
+      });
   }
 
   function go_to (slide_number) {
@@ -90,7 +157,7 @@
         '<img src="'+data.photo+'" alt="image" />'+
         '<h4>'+data.name+'</h4>'+
         '<div>'+message+'</div>'+
-        '<div class="meta">'+data.timestamp+'</div>'+
+        // '<div class="meta">'+data.timestamp+'</div>'+
         '<div class="clearfix"></div>'+
       '</li>'
     );
